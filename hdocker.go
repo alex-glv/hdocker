644685231db51cc1c902e6388d5ce1c394f62151
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-01-31 17:03:50 +0100

    Adding debug table

--- hdocker.go
+++ hdocker.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/alex-glv/hdocker/layerdraw"
 	// "github.com/alex-glv/hdocker/selectables"
-	// "fmt"
+	"fmt"
 	"time"
 )
 
@@ -23,14 +23,17 @@ Layers
 */
 
 type Node struct {
-	Prev     *Node
-	Next     *Node
-	Hash     string
-	Selected int
+	Prev      *Node
+	Next      *Node
+	Container docker.APIContainers
+	Hash      string
+	Selected  bool
 }
 
 var nodes = make(map[string]*Node)
 var tail *Node
+var head *Node
+var selectedHash string
 var endpoint = "unix:///var/run/docker.sock"
 
 func getRunningContainers() []docker.APIContainers {
@@ -44,14 +47,17 @@ func AddSelectableNode(groupNode *Node, nodes map[string]*Node) {
 	if exists {
 		return
 	}
-	nodes[groupNode.Hash] = groupNode
-	if tail == nil {
-		tail = groupNode
-	} else {
-		groupNode.Prev = tail
-		tail.Next = groupNode
+
+	if len(nodes) == 0 {
+		head = groupNode
 		tail = groupNode
 	}
+
+	groupNode.Prev = tail
+	groupNode.Next = head
+	tail.Next = groupNode
+	tail = groupNode
+	nodes[groupNode.Hash] = groupNode
 }
 
 func DeleteSelectableNode(hash string, nodes map[string]*Node) {
@@ -59,21 +65,16 @@ func DeleteSelectableNode(hash string, nodes map[string]*Node) {
 	if !e {
 		return
 	}
-	if tail == nodes[hash] {
-		tail = nodes[hash].Prev
+	if tail == tbd {
+		tail = tbd.Prev
+	}
+	if head == tbd {
+		head = tbd.Next
 	}
 	prev := tbd.Prev
 	next := tbd.Next
-	if prev != nil && next == nil {
-		prev.Next = next
-	}
-	if next != nil && prev == nil {
-		next.Prev = prev
-	}
-	if next != nil && prev != nil {
-		prev.Next = next
-		next.Prev = prev
-	}
+	prev.Next = tbd.Next
+	next.Prev = tbd.Prev
 
 	delete(nodes, hash)
 
@@ -82,24 +83,31 @@ func updateTableRows(t *layerdraw.Table, lc *layerdraw.Container, cnt []docker.A
 	foundIds := make(map[string]bool)
 
 	for _, c := range cnt {
-		lc.DeleteGroup(c.ID)
 		if _, e := nodes[c.ID]; !e {
 			cNode := &Node{
-				Prev: tail,
-				Hash: c.ID,
+				Hash:      c.ID,
+				Container: c,
 			}
 			AddSelectableNode(cNode, nodes)
+		} else {
+			nodes[c.ID].Container = c
 		}
 		foundIds[c.ID] = true
-		row := layerdraw.NewTableRow(c.ID, c.Image, c.Command, c.Status, c.Names[0])
-		lc.AddTableRow(t, row, c.ID)
 	}
-	for _, n := range nodes {
-		if _, e := foundIds[n.Hash]; !e {
-			DeleteSelectableNode(n.Hash, nodes)
-			lc.DeleteGroup(n.Hash)
+	node := head
+	for i := 0; i < len(nodes); i++ {
+		// fmt.Println(node.Hash)
+		lc.DeleteGroup(node.Hash)
+		if _, e := foundIds[node.Hash]; !e {
+			DeleteSelectableNode(node.Hash, nodes)
+
+		} else {
+			row := layerdraw.NewTableRow(node.Selected, node.Container.ID, node.Container.Image, node.Container.Command, node.Container.Status, node.Container.Names[0])
+			lc.AddTableRow(t, row, node.Container.ID)
 		}
+		node = node.Next
 	}
+
 }
 
 func drawContainersTable(width, height int) ([]string, []int) {
@@ -116,7 +124,7 @@ func main() {
 		for {
 			cnt := getRunningContainers()
 			containers_queue <- cnt
-			time.Sleep(1000 * time.Millisecond)
+			time.Sleep(100 * time.Millisecond)
 		}
 	}()
 
@@ -137,14 +145,21 @@ func main() {
 	headerElement := layerdraw.NewContainer(0, 0, width, 1)
 
 	layer := layerdraw.NewLayer()
-	layer.Add(headerElement)
 
 	headerElement.AddTableHeader(table)
-	headerElement.Draw()
 
-	rowsElement := layerdraw.NewContainer(0, 1, width-2, height)
+	rowsElement := layerdraw.NewContainer(0, 1, width-2, height-20)
 	updateTableRows(table, rowsElement, getRunningContainers(), nodes)
-	rowsElement.Draw()
+
+	debugElement := layerdraw.NewContainer(0, height-2, width, 2)
+	debugTable := layerdraw.NewTable([]string{"Selected"}, []int{width})
+	debugElement.AddTableRow(debugTable, layerdraw.NewTableRow(false, "Test"), "selHash")
+
+	layer.Add(headerElement)
+	layer.Add(debugElement)
+	layer.Add(rowsElement)
+
+	layer.Draw()
 	termbox.Flush()
 	defer termbox.Close()
 	// draw()
@@ -152,13 +167,44 @@ loop:
 	for {
 		select {
 		case ev := <-event_queue:
-			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
-				break loop
+			if ev.Type == termbox.EventKey {
+				if ev.Key == termbox.KeyEsc {
+					break loop
+				}
+
+				if ev.Key == termbox.KeyArrowDown || ev.Key == termbox.KeyArrowUp {
+					if len(nodes) == 0 {
+						break
+					}
+					var line string
+					if selected, e := nodes[selectedHash]; e {
+						if ev.Key == termbox.KeyArrowDown {
+							selected.Next.Selected = true
+							line = selected.Next.Hash
+						} else {
+							selected.Prev.Selected = true
+							line = selected.Prev.Hash
+						}
+						selected.Selected = false
+
+					} else if head != nil {
+						selectedHash = head.Hash
+						head.Selected = true
+					}
+					debugElement.DeleteGroup("selHash")
+					debugElement.DeleteGroup("headTail")
+					debugElement.AddTableRow(debugTable, layerdraw.NewTableRow(false, line), "selHash")
+					debugElement.AddTableRow(debugTable, layerdraw.NewTableRow(false, fmt.Sprintf(head.Hash, " <> ", tail.Hash)), "headTail")
+					updateTableRows(table, rowsElement, getRunningContainers(), nodes)
+					layer.Draw()
+					termbox.Flush()
+				}
+
 			}
 
 		case cnt := <-containers_queue:
 			updateTableRows(table, rowsElement, cnt, nodes)
-			rowsElement.Draw()
+			layer.Draw()
 			termbox.Flush()
 		}
 	}
